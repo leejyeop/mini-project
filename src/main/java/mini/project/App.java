49c523bfebf5cbbd2b34b85b3123dccd0ae4c073
Lee Jong-Yeap <ha901123@naver.com>
2020-09-09 11:16:25 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/leejyeop/mini-project into
    main
    
    # Conflicts:
    #	src/main/java/mini/project/App.java
    테스트

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -4,13 +4,13 @@
 package mini.project;
 
 public class App {
-  public String getGreeting() {
-    return "Hello world 가나다라마바사.";
-  }
+	public String getGreeting() {
+		return "Hello world 가나다라마바사.123";
+	}
 
-  public static void main(String[] args) {
-    System.out.println("미니프로젝트 연습");
-    System.out.println("미니 연습");
-    System.out.println();
-  }
+	public static void main(String[] args) {
+		System.out.println("미니프로젝트 연습");
+		System.out.println("미니 연습");
+		System.out.println();
+	}
 }
