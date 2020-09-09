9a1735fe34f957b81f014130419f47b0ac73f911
2seugnhyuck <rotid1818@gmail.com>
2020-09-09 10:10:47 +0900

    테스

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -5,10 +5,11 @@ package mini.project;
 
 public class App {
   public String getGreeting() {
-    return "Hello world.";
+    return "Hello world 가나다라마바사.";
   }
 
   public static void main(String[] args) {
     System.out.println("미니프로젝트 연습");
+    System.out.println("미니 연습");
   }
 }
