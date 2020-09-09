8086e3ba4bca113b5263e8c3274bf6fdfb844bf8
Lee Jong-Yeap <ha901123@naver.com>
2020-09-09 13:08:43 +0900

    테스트1308

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -4,16 +4,17 @@
 package mini.project;
 
 public class App {
-  public String getGreeting() {
-    return "Hello world 가나다라마바사222.123";
-  }
+	public String getGreeting() {
+		return "Hello world 가나다라마바사222.123";
+	}
 
-  public static void main(String[] args) {
-    System.out.println("미니프로젝트 연습");
-    System.out.println("미니 연습");
-    System.out.println("테스트.12:55");
-    System.out.println("테스트.12:58");
-    System.out.println("이승혁13:05");
+	public static void main(String[] args) {
+		System.out.println("미니프로젝트 연습");
+		System.out.println("미니 연습");
+		System.out.println("테스트.12:55");
+		System.out.println("테스트.12:58");
+		System.out.println("이승혁13:05");
+		System.out.println("가나다라마바사");
 
-  }
+	}
 }
